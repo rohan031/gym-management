8dee769ab0208c19e4368d97b3cca5ffcf3e4e4b
rohan <80212018+rohan031@users.noreply.github.com>
2022-12-22 15:23:51 +0530

    changes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
+const path = require("path");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DB_URI);
+
+app.use(express.static("client/build"));
 
 app.use(express.json());
 
@@ -13,6 +16,9 @@ app.use(express.json());
 const login = require("./routes/login");
 const members = require("./routes/member");
 
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 app.use("/login", login);
 app.use("/members", members);
 
